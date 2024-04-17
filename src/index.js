f3a96b3b56a1799e0fa462b1d679701b42c61faf
Ajay Praveen <borraajaypraveen@gmail.com>
2024-04-17 09:59:40 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import App from './App.js';
+import Terminal from './components/engineeringterminal.jsx';
+import Own from './components/extra/own.jsx';
+import Home from './components/extra/homepage.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Terminal />
+    {/* <Home /> */}
   </React.StrictMode>
 );
 
